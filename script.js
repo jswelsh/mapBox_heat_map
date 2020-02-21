f6dc92b579e3f67ced51357b32de4fb9daacf5b9
jswelsh <james.sinclair.welsh@gmail.com>
2020-02-21 10:47:44 -0800

    add access token

---
+++ script.js
@@ -0,0 +1,8 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoianN3ZWxzaCIsImEiOiJjazFqdXczOHYyNWNxM25udDE4bGh3cGozIn0.8uyxIS9Zv3y_QwVnwltlVg';
+    var map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [-79.999732, 40.4374],
+      zoom: 11
+    });
+    
